a47509c95823edb0938d277dca2ee17447ea6bf3
UnknownPlatypus <thibautdecombe2.0@gmail.com>
2020-08-03 16:52:16 +0200

    Adding progress Bar

--- add-on/script.js
+++ add-on/script.js
@@ -1,10 +1,18 @@
 document.getElementById('audio_M4a').addEventListener('click', function(){
-    browser.runtime.sendMessage("m4a")
+    browser.runtime.sendMessage("m4a");
+    document.querySelector("#progress-bar").classList.remove("hidden");
 })
 document.getElementById('audio_mp3').addEventListener('click', function(){
-    browser.runtime.sendMessage("mp3")
+    browser.runtime.sendMessage("mp3");
+    document.querySelector("#progress-bar").classList.add("hidden");
 })
 document.getElementById('video').addEventListener('click', function(){
     browser.runtime.sendMessage("mp4")
 })
 
+
+// Listen for background script message
+browser.runtime.onMessage.addListener((response) => {
+    if (response=="Complete"){
+    document.querySelector("#progress-bar").classList.add("hidden");}
+  })
