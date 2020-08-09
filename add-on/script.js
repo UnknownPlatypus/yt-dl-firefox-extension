805b0e7bf354ded02f2ed8a188dded8bbabc20aa
UnknownPlatypus <thibautdecombe2.0@gmail.com>
2020-08-09 16:10:15 +0200

    Progress Bar + Speed

--- add-on/script.js
+++ add-on/script.js
@@ -14,5 +14,18 @@ document.getElementById('video').addEventListener('click', function(){
 // Listen for background script message
 browser.runtime.onMessage.addListener((response) => {
     if (response=="Complete"){
-    document.querySelector("#progress-bar").classList.add("hidden");}
+        document.querySelector("#progress-bar").classList.add("hidden");
+    }
+    if (response[0]=="PROG"){
+        percent=response[1];
+        percentFloat=percent.substring(0,response[1].length-1);
+        float_val=parseFloat(percentFloat);
+        sizeDL=response[2];
+        sizeDLFloat=sizeDL.substring(0,sizeDL-3)
+        valSpeed="Downloading... "+ response[3];
+        document.querySelector("#prog").value=float_val;
+        document.querySelector("#perc").innerHTML=percent + " / "+ sizeDL;
+        document.querySelector("#speed").innerHTML=valSpeed;
+    }
+
   })
