7522cf8f18e4d45940bb0e08144c26b972348e14
Thibaut Decombe <thibaut.decombe@ecl17.ec-lyon.fr>
2020-09-17 19:07:48 +0200

    Update opt-in

--- add-on/script.js
+++ add-on/script.js
@@ -1,3 +1,27 @@
+// Addon Opt-in actions
+window.addEventListener("load", function() {
+    // set up the appearance of the popup depending on the outcome of the opt-in
+    browser.storage.local.get("optInShown", function(result) {
+      console.log("Setting up UI. result.optInShown:" + result.optInShown);
+      document.getElementById("opt-in-prompt").hidden = result.optInShown;
+      document.getElementById("after-opt-in").hidden = !result.optInShown;
+    });
+  
+    document.getElementById("button-enable").addEventListener(
+      "click",
+      function() {
+        browser.storage.local.set({ "optIn" : true, "optInShown" : true });
+        window.close();
+    });
+  
+    document.getElementById("button-cancel").addEventListener(
+      "click",
+      function() {
+        browser.storage.local.set({ "optIn" : false, "optInShown" : false });
+        window.close();
+    });
+  });
+  
 ////////// Plugin Event listeners //////////
 // 3 Main formats
 document.getElementById('audio_M4a').addEventListener('click', function(){
